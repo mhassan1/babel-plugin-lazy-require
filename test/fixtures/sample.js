fd03e7fcc774b2daf829dfdda1478a11edbe8914
mhassan1 <marc.j.hassan@gmail.com>
2023-07-05 16:28:31 -0400

    chore: bump dependencies

--- test/fixtures/sample.js
+++ test/fixtures/sample.js
@@ -1,192 +1,149 @@
 const _constGlobalRequire = {
-    initialized: false
+  initialized: false
 };
 const _letGlobalRequire = {
-    initialized: false
+  initialized: false
 };
 const _varGlobalRequire = {
-    initialized: false
+  initialized: false
 };
 const _constWrappedRequire = {
-    initialized: false
+  initialized: false
 };
 const _letWrappedRequire = {
-    initialized: false
+  initialized: false
 };
 const _destructured = {
-    initialized: false
+  initialized: false
 };
 const _destructuredAfterRename = {
-    initialized: false
+  initialized: false
 };
 const _nonglobalRequire = {
-    initialized: false
+  initialized: false
 };
 const _imports = {
-    get constGlobalRequire() {
-        if (!_constGlobalRequire.initialized) {
-            _constGlobalRequire.value = require("const-global-require");
-            _constGlobalRequire.initialized = true;
-        }
-
-        return _constGlobalRequire.value;
-    },
-
-    get letGlobalRequire() {
-        if (!_letGlobalRequire.initialized) {
-            _letGlobalRequire.value = require("let-global-require");
-            _letGlobalRequire.initialized = true;
-        }
-
-        return _letGlobalRequire.value;
-    },
-
-    set letGlobalRequire(value) {
-        if (!_letGlobalRequire.initialized) {
-            require("let-global-require");
-
-            _letGlobalRequire.initialized = true;
-        }
-
-        _letGlobalRequire.value = value;
-    },
-
-    get varGlobalRequire() {
-        if (!_varGlobalRequire.initialized) {
-            _varGlobalRequire.value = require("var-global-require");
-            _varGlobalRequire.initialized = true;
-        }
-
-        return _varGlobalRequire.value;
-    },
-
-    set varGlobalRequire(value) {
-        if (!_varGlobalRequire.initialized) {
-            require("var-global-require");
-
-            _varGlobalRequire.initialized = true;
-        }
-
-        _varGlobalRequire.value = value;
-    },
-
-    get constWrappedRequire() {
-        if (!_constWrappedRequire.initialized) {
-            _constWrappedRequire.value = noop(require("const-wrapped-require"));
-            _constWrappedRequire.initialized = true;
-        }
-
-        return _constWrappedRequire.value;
-    },
-
-    get letWrappedRequire() {
-        if (!_letWrappedRequire.initialized) {
-            _letWrappedRequire.value = noop(require("let-wrapped-require"));
-            _letWrappedRequire.initialized = true;
-        }
-
-        return _letWrappedRequire.value;
-    },
-
-    set letWrappedRequire(value) {
-        if (!_letWrappedRequire.initialized) {
-            noop(require("let-wrapped-require"));
-            _letWrappedRequire.initialized = true;
-        }
-
-        _letWrappedRequire.value = value;
-    },
-
-    get destructured() {
-        if (!_destructured.initialized) {
-            _destructured.value = noop(require("let-global-require-destructured-object")).destructured;
-            _destructured.initialized = true;
-        }
-
-        return _destructured.value;
-    },
-
-    set destructured(value) {
-        if (!_destructured.initialized) {
-            noop(require("let-global-require-destructured-object")).destructured;
-            _destructured.initialized = true;
-        }
-
-        _destructured.value = value;
-    },
-
-    get destructuredAfterRename() {
-        if (!_destructuredAfterRename.initialized) {
-            _destructuredAfterRename.value = noop(require("let-global-require-destructured-object")).destructuredBeforeRename;
-            _destructuredAfterRename.initialized = true;
-        }
-
-        return _destructuredAfterRename.value;
-    },
-
-    set destructuredAfterRename(value) {
-        if (!_destructuredAfterRename.initialized) {
-            noop(require("let-global-require-destructured-object")).destructuredBeforeRename;
-            _destructuredAfterRename.initialized = true;
-        }
-
-        _destructuredAfterRename.value = value;
-    },
-
-    get nonglobalRequire() {
-        if (!_nonglobalRequire.initialized) {
-            _nonglobalRequire.value = require("nonglobal-require");
-            _nonglobalRequire.initialized = true;
-        }
-
-        return _nonglobalRequire.value;
+  get constGlobalRequire() {
+    if (!_constGlobalRequire.initialized) {
+      _constGlobalRequire.value = require("const-global-require");
+      _constGlobalRequire.initialized = true;
     }
-
+    return _constGlobalRequire.value;
+  },
+  get letGlobalRequire() {
+    if (!_letGlobalRequire.initialized) {
+      _letGlobalRequire.value = require("let-global-require");
+      _letGlobalRequire.initialized = true;
+    }
+    return _letGlobalRequire.value;
+  },
+  set letGlobalRequire(value) {
+    if (!_letGlobalRequire.initialized) {
+      require("let-global-require");
+      _letGlobalRequire.initialized = true;
+    }
+    _letGlobalRequire.value = value;
+  },
+  get varGlobalRequire() {
+    if (!_varGlobalRequire.initialized) {
+      _varGlobalRequire.value = require("var-global-require");
+      _varGlobalRequire.initialized = true;
+    }
+    return _varGlobalRequire.value;
+  },
+  set varGlobalRequire(value) {
+    if (!_varGlobalRequire.initialized) {
+      require("var-global-require");
+      _varGlobalRequire.initialized = true;
+    }
+    _varGlobalRequire.value = value;
+  },
+  get constWrappedRequire() {
+    if (!_constWrappedRequire.initialized) {
+      _constWrappedRequire.value = noop(require("const-wrapped-require"));
+      _constWrappedRequire.initialized = true;
+    }
+    return _constWrappedRequire.value;
+  },
+  get letWrappedRequire() {
+    if (!_letWrappedRequire.initialized) {
+      _letWrappedRequire.value = noop(require("let-wrapped-require"));
+      _letWrappedRequire.initialized = true;
+    }
+    return _letWrappedRequire.value;
+  },
+  set letWrappedRequire(value) {
+    if (!_letWrappedRequire.initialized) {
+      noop(require("let-wrapped-require"));
+      _letWrappedRequire.initialized = true;
+    }
+    _letWrappedRequire.value = value;
+  },
+  get destructured() {
+    if (!_destructured.initialized) {
+      _destructured.value = noop(require("let-global-require-destructured-object")).destructured;
+      _destructured.initialized = true;
+    }
+    return _destructured.value;
+  },
+  set destructured(value) {
+    if (!_destructured.initialized) {
+      noop(require("let-global-require-destructured-object")).destructured;
+      _destructured.initialized = true;
+    }
+    _destructured.value = value;
+  },
+  get destructuredAfterRename() {
+    if (!_destructuredAfterRename.initialized) {
+      _destructuredAfterRename.value = noop(require("let-global-require-destructured-object")).destructuredBeforeRename;
+      _destructuredAfterRename.initialized = true;
+    }
+    return _destructuredAfterRename.value;
+  },
+  set destructuredAfterRename(value) {
+    if (!_destructuredAfterRename.initialized) {
+      noop(require("let-global-require-destructured-object")).destructuredBeforeRename;
+      _destructuredAfterRename.initialized = true;
+    }
+    _destructuredAfterRename.value = value;
+  },
+  get nonglobalRequire() {
+    if (!_nonglobalRequire.initialized) {
+      _nonglobalRequire.value = require("nonglobal-require");
+      _nonglobalRequire.initialized = true;
+    }
+    return _nonglobalRequire.value;
+  }
 };
-
 require('static-require');
-
 const constWrappedRequireWithArgs = noop(require('const-wrapped-require'), 'data');
-
 const [destructuredItem] = require('const-global-destructured-array');
-
 const obj = {
-    letGlobalRequire: () => {
-        _imports.constGlobalRequire.prop = 5;
-
-        fns[_imports.constGlobalRequire]();
-
-        _imports.letGlobalRequire = {};
-
-        try {
-            obj.constGlobalRequire = _imports.varGlobalRequire;
-        } catch (letGlobalRequire) {}
-
-        _imports.destructured();
-
-        _imports.destructuredAfterRename();
-
-        closure2(letWrappedRequire => {
-            closure1(() => {
-                letWrappedRequire();
-            });
-        });
-    },
-
-    varGlobalRequire() {
-        function varGlobalRequire() {
-            const varGlobalRequire = {
-                foo: '13'
-            };
-            const x = varGlobalRequire;
-        }
-
-        const x = varGlobalRequire;
-
-        class constGlobalRequire {
-            letGlobalRequire() {}
-
-        }
+  letGlobalRequire: () => {
+    _imports.constGlobalRequire.prop = 5;
+    fns[_imports.constGlobalRequire]();
+    _imports.letGlobalRequire = {};
+    try {
+      obj.constGlobalRequire = _imports.varGlobalRequire;
+    } catch (letGlobalRequire) {}
+    _imports.destructured();
+    _imports.destructuredAfterRename();
+    closure2(letWrappedRequire => {
+      closure1(() => {
+        letWrappedRequire();
+      });
+    });
+  },
+  varGlobalRequire() {
+    function varGlobalRequire() {
+      const varGlobalRequire = {
+        foo: '13'
+      };
+      const x = varGlobalRequire;
     }
-
+    const x = varGlobalRequire;
+    class constGlobalRequire {
+      letGlobalRequire() {}
+    }
+  }
 };
